1c57ec18670e40dc372fec56dc0149632771ee02
Mikolaj <szymikolaj@gmail.com>
2021-12-16 16:30:42 +0100

    Shop item details renderer

---
+++ src/components/longDescription/LongDescription.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function LongDescription() {
+    return (
+        <div>
+            this is long description
+        </div>
+    )
+}
--- src/components/shopItemDetails/ShopItemDetails.js
+++ src/components/shopItemDetails/ShopItemDetails.js
@@ -1,14 +1,61 @@
-import React from "react"
+import React, { useState } from "react"
 import { renderRichText } from "gatsby-source-contentful/rich-text"
+
+import { AiOutlineOrderedList } from "react-icons/ai"
+import { BsFileText } from "react-icons/bs";
+import { CgMoreO } from "react-icons/cg";
+
 import TableOfContents from "../tableOfContents/TableOfContents"
 
+import "./shopItemDetails.css"
+import LongDescription from "../longDescription/LongDescription"
+
+const details_items = [
+  { label: "Spis treści", icon: <AiOutlineOrderedList /> },
+  { label: "Opis", icon: <BsFileText /> },
+  { label: "Szczegóły", icon: <CgMoreO /> },
+]
 
 export default function ShopItemDetails({
   tableOfContents,
   longDescription,
   details,
 }) {
+  const [selectedMenuItemIndex, setSelectedMenuItemIndex] = useState(0)
   const tableOfContentOutput = renderRichText(tableOfContents)
 
-  return <TableOfContents tableOfContents={tableOfContentOutput} />
+  const renderSwitch = param => {
+    switch (param) {
+      case 0:
+        return <TableOfContents tableOfContents={tableOfContentOutput} />
+        break
+      case 1:
+        return <LongDescription />
+        break
+      case 2:
+        return "szczegóły"
+        break
+    }
+  }
+
+  return (
+    <>
+      <ul className="details-menu-list">
+        {details_items.map((item, index) => {
+          return (
+            <li
+              className={selectedMenuItemIndex === index ? "active" : ""}
+              onClick={e => setSelectedMenuItemIndex(index)}
+            >
+              <i>{item.icon}</i>
+              <span>{item.label}</span>
+            </li>
+          )
+        })}
+      </ul>
+      <div className="details-tab-renderer">
+        {renderSwitch(selectedMenuItemIndex)}
+      </div>
+    </>
+  )
 }
